3132c5754c3c5df2d5d00be85fb86bc30e9658bc
spetrov <s.petrov@live.com>
2022-08-17 22:42:03 +0300

    Finished video 171

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,20 @@ import storage from "redux-persist/lib/storage";
 
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
 // const middleWares = [process.env.NODE_ENV !== "production" ?? logger].filter(
 //   Boolean
 // );
-const middleWares = [logger];
+const middleWares = [logger, thunk];
+
+const thunkMiddleware = (store) => (next) => (action) => {
+  if(typeof(action) === "function") {
+    action(dispatch);
+  }
+}
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
@@ -18,7 +25,7 @@ const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 export const persistedReducer = persistReducer(persistConfig, rootReducer);
